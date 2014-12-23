d994ba6661f3975f992580e1ff2ac8c9accf635b
Jesse Farmer <jessethebuilder@yahoo.com>
2014-12-22 20:59:17 -0800

    Calendar Week UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@ g = Gym.new :name => "Kevin's Gym", :phone => "360-555-1212", :email => "gym@tes
 g.address = Address.new :street => '4218 S. Mt. Angeles Rd', :city => 'Port Angeles', :state => 'wa', :zip => '98362'
 g.save!
 
+4.times do
+  u = User.new :first_name => Faker::Name.first_name, :last_name => Faker::Name.last_name,
+               :email => Faker::Internet.email, :password => 'carltonlasiter', :level => :staff
+  u.save!
+end
+
 30.times do
   hour = Random.rand(6..20)
   if hour >= 12
@@ -20,5 +26,7 @@ g.save!
 
   e = Event.new :name => Faker::Company.name, :starts_at => time, :ends_at => time + [15, 30, 45, 60, 75, 90].sample.minutes,
                 :event_type => :class, :description => Faker::Lorem.sentences(sentence_count = 5).join(' ')
+  e.user = User.all.sample
   e.save!
+
 end
