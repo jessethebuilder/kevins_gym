6aecc7781b0af76cb29aa33477ffd5ff622a6eb7
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-06 13:35:53 -0800

    Revisit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306091839) do
+ActiveRecord::Schema.define(version: 20150306212129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,8 @@ ActiveRecord::Schema.define(version: 20150306091839) do
     t.string   "instagram_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "leafly_app_id"
+    t.string   "leafly_app_key"
   end
 
   create_table "users", force: true do |t|
