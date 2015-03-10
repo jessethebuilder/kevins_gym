29c4a0513eb412c6165cdfa4f3d96147e618fb49
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-10 00:40:17 -0700

    Revisit

--- app/controllers/news_stories_controller.rb
+++ app/controllers/news_stories_controller.rb
@@ -1,20 +1,11 @@
 class NewsStoriesController < ApplicationController
-
   before_action :set_news_story, only: [:show, :edit, :update, :destroy]
-
-  before_action :except => [:index, :show, :more] do |controller|
-    authenticate_user_level!(controller, 'admin')
-  end
+  before_action :set_upcoming_classes, only: [:index]
 
   respond_to :html, :js
 
-  def more
-    @news_stories = NewsStory.published
-  end
-
   def index
-    set_upcoming_classes
-    @news_stories = NewsStory.published.order('updated_at DESC').limit(6)
+    @news_stories = NewsStory.published.order('updated_at DESC')
     respond_with(@news_stories)
   end
 
@@ -32,18 +23,24 @@ class NewsStoriesController < ApplicationController
 
   def create
     @news_story = NewsStory.new(news_story_params)
-
-    publish_or_archive(@news_story)
-
     @news_story.save
+
+    #this is the only change in the controller required for SaveDraftArchiveDelete (here and :update)
+    #todo move this and #parse_commit to an extend maybe
+    @news_story.commit = parse_commit
+
+
+    # @news_story.save
+
+
     respond_with(@news_story)
   end
 
   def update
-    publish_or_archive(@news_story)
-
 
     @news_story.update(news_story_params)
+    @news_story.commit = parse_commit
+
     respond_with(@news_story)
   end
 
@@ -54,25 +51,28 @@ class NewsStoriesController < ApplicationController
   end
 
   private
-    def set_news_story
-      if p = params[:id]
-        @news_story = NewsStory.find(p)
-      else
-        @news_story = NewsStory.new
-      end
-    end
+  def set_news_story
+    # if p = params[:id]
+    @news_story = NewsStory.find(params[:id])
+    # else
+    #   @news_story = NewsStory.new
+    # end
+  end
 
-    def news_story_params
-      params.require(:news_story).permit(:title, :content, :author_id, :published, :archived,
-                                         :bootsy_image_gallery_id,
-                                         :main_news_story_image, :main_news_story_image_cache, :remote_main_news_story_image_url)
+  def parse_commit
+    case params[:commit]
+      when 'Save Draft'
+        :draft
+      when 'Publish'
+        :publish
+      when 'Archive'
+        :archive
     end
+  end
 
-    #def publish?
-    #  params[:commit] == 'Publish'
-    #end
-    #
-    #def archive?
-    #  params[:commit] == 'Archive'
-    #end
+  def news_story_params
+    params.require(:news_story).permit(:title, :content,
+                                       :bootsy_image_gallery_id,
+                                       :main_news_story_image, :main_news_story_image_cache, :remote_main_news_story_image_url)
+  end
 end
